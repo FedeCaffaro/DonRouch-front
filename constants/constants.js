b611ac2ec4fad4ca02868ea68da2ecdfcd531e80
FedeCaffaro <fcaffaro@itba.edu.ar>
2022-03-18 10:52:57 -0300

    first commit

---
+++ constants/constants.js
@@ -0,0 +1,6 @@
+const abi = require("./DonRouchABI.json");
+
+const contractAddress = "0x8f4bEAb50E5a6c3a8478Bb8acB393BF19Fec3fAb";
+
+console.log(abi);
+module.exports = {abi,contractAddress};
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { ethers } = require("ethers");
+const { abi, contractAddress } =  require("./constants/constants");
 
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
@@ -18,101 +19,11 @@ async function connect() {
 
 async function execute() {
   if (typeof window.ethereum !== "undefined") {
-    contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-    const abi = [
-      {
-        inputs: [
-          {
-            internalType: "string",
-            name: "_name",
-            type: "string",
-          },
-          {
-            internalType: "uint256",
-            name: "_favoriteNumber",
-            type: "uint256",
-          },
-        ],
-        name: "addPerson",
-        outputs: [],
-        stateMutability: "nonpayable",
-        type: "function",
-      },
-      {
-        inputs: [
-          {
-            internalType: "string",
-            name: "",
-            type: "string",
-          },
-        ],
-        name: "nameToFavoriteNumber",
-        outputs: [
-          {
-            internalType: "uint256",
-            name: "",
-            type: "uint256",
-          },
-        ],
-        stateMutability: "view",
-        type: "function",
-      },
-      {
-        inputs: [
-          {
-            internalType: "uint256",
-            name: "",
-            type: "uint256",
-          },
-        ],
-        name: "people",
-        outputs: [
-          {
-            internalType: "uint256",
-            name: "favoriteNumber",
-            type: "uint256",
-          },
-          {
-            internalType: "string",
-            name: "name",
-            type: "string",
-          },
-        ],
-        stateMutability: "view",
-        type: "function",
-      },
-      {
-        inputs: [],
-        name: "retrieve",
-        outputs: [
-          {
-            internalType: "uint256",
-            name: "",
-            type: "uint256",
-          },
-        ],
-        stateMutability: "view",
-        type: "function",
-      },
-      {
-        inputs: [
-          {
-            internalType: "uint256",
-            name: "_favoriteNumber",
-            type: "uint256",
-          },
-        ],
-        name: "store",
-        outputs: [],
-        stateMutability: "nonpayable",
-        type: "function",
-      },
-    ];
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     const contract = new ethers.Contract(contractAddress, abi, signer);
     try {
-      await contract.store(42);
+      await contract.claimItem([1,2],[1,1]);
     } catch (error) {
       console.log(error);
     }
